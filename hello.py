7841e82f1d4876a3452a062756ded6f0bf219104
shubham <shubhamraj12114@gmail.com>
2026-04-24 21:51:55 +0530

    python files added

---
+++ hello.py
@@ -0,0 +1,6 @@
+class def():
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"
---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+def main():
+    print("Hello World!")
+    print("This is a sample Python program.")
+    print("The current working directory is:", os.getcwd())
